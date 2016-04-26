402f2d37b95f2dc3cdb99548d325b24a5946e6a6
James Cat <james@catplusplus.org.uk>
2016-04-26 16:01:31 +0100

    navigation text size styling and tidy up javascript for both staff and student and main nav

--- sites/themes/nucleare-wpcom/footer.php
+++ sites/themes/nucleare-wpcom/footer.php
@@ -20,14 +20,33 @@
   <?php wp_footer(); ?>
 
   <script>
-  jQuery(function(){
-    jQuery('.charm-menu').click(function(){
-      jQuery( 'body' ).toggleClass( "menu-open" );
-    });
-  });
 
-  jQuery(".dropdown-nav, .dropdown-nav touchButton").click(function(e)
-  {
+
+
+  jQuery(function(){
+
+
+    siteUIClosing = function() {
+      jQuery("body").addClass("site-ui-closing"),
+      setTimeout(function() {
+        jQuery("body").removeClass("site-ui-closing")
+      }, 1e3)
+    }
+
+    jQuery('.charm-menu').on("click", function() {
+      jQuery("body").toggleClass("menu-open");
+      jQuery("body").toggleClass("menu-closed");
+      jQuery("body").removeClass("staff-students-open");
+      jQuery("body").addClass("staff-students-closed");
+      jQuery("#student-nav").attr("aria-hidden", "true");
+
+      jQuery("body").hasClass("menu-closed") ?
+          ( jQuery("#primary-nav").attr("aria-hidden", "true"), siteUIClosing() ) : 
+          ( jQuery("body").hasClass("tabbing") && setTimeout(function() { jQuery("#primary-nav").find("li > a").eq(0).focus() }, 500), jQuery("#primary-nav").attr("aria-hidden", "false") )
+    });
+
+    jQuery(".dropdown-nav, .dropdown-nav touchButton").click(function(e)
+    {
       jQuery(".breadcrumb-wrapper").toggleClass("active");
 
       setTimeout(
@@ -36,9 +55,25 @@
           jQuery(".breadcrumb .secondary-nav").toggleClass("open");
           jQuery(".dropdown-nav").toggleClass("open");
         },50);
+    });
+
+    jQuery(".header__charm--text").on("click", function() {
+
+      jQuery("body").toggleClass("staff-students-open"),
+      jQuery("body").toggleClass("staff-students-closed"),
+
+      jQuery("body").removeClass("menu-open"),  // if the regular menu is open, close it.
+      jQuery("body").addClass("menu-closed"),
+
+      jQuery("#primary-nav").attr("aria-hidden", "true"),
+
+      jQuery("body").hasClass("staff-students-closed") ?
+          ( jQuery("#student-nav").attr("aria-hidden", "true"), siteUIClosing() ) :
+          ( jQuery("body").hasClass("tabbing") && setTimeout( function() { jQuery("#student-nav").find("li > a").eq(0).focus() }, 500 ),jQuery("#student-nav").attr("aria-hidden", "false") );
+
+    });
+
   });
-
-
   </script>
 
 </body>
