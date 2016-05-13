96d8e9964652b363cc909d898571308a25d3585e
James Cat <james@catplusplus.org.uk>
2016-05-13 11:18:45 +0100

    add 'blog home' link to hero text

--- sites/themes/nucleare-wpcom/partials/goldhero.php
+++ sites/themes/nucleare-wpcom/partials/goldhero.php
@@ -3,7 +3,7 @@
   <div class="hero hero--simple" style="background-image: url(<?php header_image(); ?>)">
       <div class="hero__wrapper wrapper">
           <div class="hero__content">
-              <h1 class="hero__heading" style="color: #<?php echo get_header_textcolor(); ?>"><?php bloginfo( 'name' ); ?></h1>
+              <a href='<?php echo get_site_url(); ?>'><h1 class="hero__heading" style="color: #<?php echo get_header_textcolor(); ?>"><?php bloginfo( 'name' ); ?></h1></a>
           </div>
       </div>
   </div> <!-- #hero -->
